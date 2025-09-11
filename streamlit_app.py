1cb396fd3b2e05e44edc4e913b8c2d7ee51ee920
MiracleOnosemudiana <onosemudianamiracle@gmail.com>
2025-09-11 07:15:19 +0100

    update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ MODEL_DIR = os.path.join(os.path.dirname(__file__), "models")
 
 @st.cache_resource
 def load_model():
-    lin_mod = joblib.load(os.path.join(MODEL_DIR, "laptop_price_model.pkl"))
-    features = joblib.load(os.path.join(MODEL_DIR, "model_features.pkl"))
-    return lin_mod, features
+    lin_reg_mod = joblib.load(os.path.join(MODEL_DIR, "laptop_price_model.kl"))
+    model_features = joblib.load(os.path.join(MODEL_DIR, "model_features.pkl"))
+    return lin_reg_mod, model_features
 
 lin_reg_mod, model_features = load_model()
 
